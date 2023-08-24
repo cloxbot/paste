da2f2e13a246ee367a53c9d6d08db45deed95584
cloxbot <64756295+cloxbot@users.noreply.github.com>
2023-08-24 07:47:49 +0300

    a

--- middleware.js
+++ middleware.js
@@ -2,19 +2,29 @@ const User = require('./models/user.js'); // Replace with the actual path to you
 
 module.exports = {
     isAuthenticated: async (req, res, next) => {
+        console.log("Running isAuthenticated middleware");
+        
         if (req.session.userId) {
+            console.log("Session userId found:", req.session.userId);
+            
             try {
                 req.currentUser = await User.findById(req.session.userId);
-                if (!req.currentUser) {
-                    console.error("User not found with ID:", req.session.userId);
+                
+                if (req.currentUser) {
+                    console.log("User fetched:", req.currentUser);
+                } else {
+                    console.error("No user found with ID:", req.session.userId);
                 }
+                
                 req.isAuthenticated = true;
             } catch (error) {
                 console.error("Error fetching user:", error.message);
             }
         } else {
+            console.log("No session userId found");
             req.isAuthenticated = false;
         }
+        
         next();
     }
 };
